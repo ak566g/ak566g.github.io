3606ca4c8d4d6cf67211bf982482e13f3868b15e
ak566g <anki01.g@gmail.com>
2021-02-10 21:39:31 +0530

    js added

---
+++ js/data.js
@@ -0,0 +1,25 @@
+var data = {
+    name : 'Ankita Gupta',
+    bio: [
+      {name: 'Name', value: 'Ankita Gupta'},
+      {name: 'Email', value: 'anki01.g@gmail.com'},
+      {name: 'Phone', value: '+91-8953XXXXXX'},
+      {name: 'Country', value: 'India'},
+      {name: 'City', value: 'Prayagraj'}
+    ],
+    github: 'ak566g',
+    projects: [
+      {name: 'Todo-List', url: 'https://github.com/ak566g/ToDo-List'},
+      {name: 'Keeper-App', url: 'https://github.com/ak566g/Keeper-App'},
+      {name: 'Hackathon Website', url: 'https://github.com/ak566g/hackathon_website'},
+      {name: 'Image Caption Generator', url: 'https://github.com/ak566g/Caption-It'},
+      {name: 'NoteBook WebApp', url: 'https://github.com/ak566g/NoteBook'},
+    ],
+    resume: 'ankita_gupta.pdf',
+    socials: [
+      {name: 'LinkedIn', url: 'https://linkedin/in/ak566g'},
+      {name: 'GitHub', url: 'https://github.com/ak566g'},
+      {name: 'Instagram', url: 'https://instagram/missankita01'},
+      {name: 'Website', url: 'under construction'}
+    ]
+  }
